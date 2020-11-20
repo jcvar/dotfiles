5a73b28ab280057ef16e85b6c1169f59cff4abc9
jcvar <36166613+jcvar@users.noreply.github.com>
2020-11-20 14:48:33 -0500

    Add symlinks to run.sh

--- run.sh
+++ run.sh
@@ -5,4 +5,10 @@
 	brew bundle ||
 	exit 1
 
-# 
+# Make config symlinks
+ln -s ~/code/dotfiles/.zshrc ~/.zshrc
+mkdir ~/.config
+ln -s ~/code/dotfiles/git ~/.config/git
+ln -s ~/code/dotfiles/alacritty  ~/.config/alacritty
+ln -s ~/code/dotfiles/nvim  ~/.config/nvim
+
