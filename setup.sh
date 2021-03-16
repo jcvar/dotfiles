980d345a6299e1601536f32261c5d17edd060a39
jcvar <36166613+jcvar@users.noreply.github.com>
2021-03-16 00:04:41 -0500

    Move paq (test token)

--- setup.sh
+++ setup.sh
@@ -5,10 +5,6 @@
     brew bundle ||
     exit 1
 
-# Install paq
-git clone https://github.com/savq/paq-nvim.git \
-    "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/pack/paqs/opt/paq-nvim
-
 # Make config symlinks
 ln -s ~/code/dotfiles/.zshrc ~/.zshrc
 mkdir ~/.config
@@ -16,3 +12,6 @@ ln -s ~/code/dotfiles/git ~/.config/git
 ln -s ~/code/dotfiles/alacritty  ~/.config/alacritty
 ln -s ~/code/dotfiles/nvim  ~/.config/nvim
 
+# Install paq
+git clone https://github.com/savq/paq-nvim.git \
+    "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/pack/paqs/opt/paq-nvim
