81a6c6c42db1323ba066778aa3da752816a53b51
jcvar <36166613+jcvar@users.noreply.github.com>
2021-06-26 23:20:50 -0500

    Organize plugins, ensure TS grammars

--- nvim/lua/init.lua
+++ nvim/lua/init.lua
@@ -1,22 +1,35 @@
-vim.cmd 'packadd paq-nvim'
 require 'paq-nvim' {
-    {'savq/paq-nvim', opt=true};   -- Let Paq manage itself
-    'savq/melange';
-    'ayu-theme/ayu-vim';
+    'savq/paq-nvim';   -- Let Paq manage itself
+    -- Utils
     'mattn/emmet-vim';
     'tpope/vim-commentary';
     'nvim-treesitter/nvim-treesitter';
+    -- LSP
     'neovim/nvim-lspconfig';
+    -- Langs
     'rust-lang/rust.vim';
+    -- Themes
+    {'savq/melange', branch='rewrite'};
+    'ayu-theme/ayu-vim';
 }
 
 require('nvim-treesitter.configs').setup{
     highlight = {
         enable = true,
     },
+    ensure_installed = {
+        'c';
+        'dart';
+        'javascript';
+        'typescript';
+        'lua';
+        'python';
+        'rust';
+        'toml';
+    }
 }
 
 local lsp = require('lspconfig')
 lsp.rls.setup{}
-vim.cmd "au BufWritePre *.rs lua vim.lsp.buf.formatting_sync()"
+vim.cmd "au BufWritePre *.rs lua vim.lsp.buf.formatting_sync()" -- rust lsp autoformat
 
