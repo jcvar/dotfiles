424efddb56ed53828ed0afe5a4e169358ddbc0e9
jcvar <36166613+jcvar@users.noreply.github.com>
2021-06-26 23:58:45 -0500

    Symlink function

--- ln-s.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Make config symlinks
-ln -s ~/code/dotfiles/.zshrc ~/.zshrc
-mkdir ~/.config
-ln -s ~/code/dotfiles/git ~/.config/git
-ln -s ~/code/dotfiles/alacritty  ~/.config/alacritty
-ln -s ~/code/dotfiles/nvim  ~/.config/nvim
--- setup.sh
+++ setup.sh
@@ -9,3 +9,12 @@
 # Install paq
 git clone https://github.com/savq/paq-nvim.git \
     "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/pack/paqs/opt/paq-nvim
+
+# Make config symlinks
+function symlinks(){
+    ln -s ~/code/dotfiles/.zshrc ~/.zshrc
+    mkdir ~/.config
+    ln -s ~/code/dotfiles/git ~/.config/git
+    ln -s ~/code/dotfiles/alacritty  ~/.config/alacritty
+    ln -s ~/code/dotfiles/nvim  ~/.config/nvim
+}
