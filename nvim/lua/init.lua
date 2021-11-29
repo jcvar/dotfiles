13d11cd7bd14e58d32acda94eedf4b545afb7372
jcvar <36166613+jcvar@users.noreply.github.com>
2021-11-29 00:18:32 -0500

    Add plugin snippets

--- nvim/lua/init.lua
+++ nvim/lua/init.lua
@@ -14,9 +14,13 @@ require 'paq-nvim' {
     -- nvim-cmp
     'hrsh7th/cmp-buffer';
     'hrsh7th/cmp-cmdline';
+    'hrsh7th/cmp-nvim-lua';
     'hrsh7th/cmp-nvim-lsp';
     'hrsh7th/cmp-path';
     'hrsh7th/nvim-cmp';
+    -- snippets
+    'L3MON4D3/LuaSnip';
+    'saadparwaiz1/cmp_luasnip';
 }
 
 -- tree-sitter config
