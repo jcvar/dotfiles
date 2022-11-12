b866028a74a8ac92ec79e4a2e19828233ec9be1b
jcvar <36166613+jcvar@users.noreply.github.com>
2022-11-12 10:35:48 -0500

    default_capabilities

--- nvim/lua/init.lua
+++ nvim/lua/init.lua
@@ -49,7 +49,7 @@ vim.g.markdown_fenced_languages = {
 
 -- lsp config
 local lsp = require('lspconfig')
-local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
+local capabilities = require('cmp_nvim_lsp').default_capabilities(vim.lsp.protocol.make_client_capabilities())
 capabilities.textDocument.completion.completionItem.snippetSupport = true
 
 attach = function()
