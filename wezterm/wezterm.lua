2ea83c6c182278f92b49caa5cfc3e5d93e27df96
jcvar <36166613+jcvar@users.noreply.github.com>
2023-05-19 00:14:35 -0500

    Use Twilio Sans Mono without ligatures

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,6 +1,7 @@
 local wezterm = require 'wezterm'
 return {
-    font = wezterm.font('SF Mono');
+    font = wezterm.font('Twilio Sans Mono');
     font_size = 14.0;
+    harfbuzz_features = { 'calt=0', 'clig=0', 'liga=0' };
     color_scheme = 'ayu';
 }
