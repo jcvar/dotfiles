fecf5c097c5a412d269db1c565f3143c8c3773bd
jcvar <36166613+jcvar@users.noreply.github.com>
2023-05-20 00:18:22 -0500

    JuliaMono

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,6 +1,6 @@
 local wezterm = require 'wezterm'
 return {
-    font = wezterm.font('Twilio Sans Mono');
+    font = wezterm.font('JuliaMono');
     font_size = 14.0;
     harfbuzz_features = { 'calt=0', 'clig=0', 'liga=0' };
     color_scheme = 'ayu';
