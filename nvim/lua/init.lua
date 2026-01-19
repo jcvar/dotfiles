356d4d21aa0b777728831280d0e349c6551cab2e
jcvar <36166613+jcvar@users.noreply.github.com>
2026-01-19 18:40:07 -0500

    Remove outdated nvim config
    - Remove lspconfig
    - Remove cmp
    - Remove unused plugins
    - Add Markdown to tree-sitter

--- nvim/lua/init.lua
+++ nvim/lua/init.lua
@@ -1,26 +1,9 @@
 require 'paq' {
     'savq/paq-nvim';   -- Let Paq manage itself
-    -- Utils
+    'savq/melange-nvim';
+    { 'nvim-treesitter/nvim-treesitter', build = ':TSUpdate' };
     'mattn/emmet-vim';
     'tpope/vim-commentary';
-    'nvim-treesitter/nvim-treesitter';
-    -- LSP
-    'neovim/nvim-lspconfig';
-    -- Langs
-    'rust-lang/rust.vim';
-    -- Themes
-    'savq/melange-nvim';
-    'ayu-theme/ayu-vim';
-    -- nvim-cmp
-    'hrsh7th/cmp-buffer';
-    'hrsh7th/cmp-cmdline';
-    'hrsh7th/cmp-nvim-lua';
-    'hrsh7th/cmp-nvim-lsp';
-    'hrsh7th/cmp-path';
-    'hrsh7th/nvim-cmp';
-    -- snippets
-    'L3MON4D3/LuaSnip';
-    'saadparwaiz1/cmp_luasnip';
 }
 
 -- tree-sitter config
@@ -34,7 +17,8 @@ require('nvim-treesitter.configs').setup{
         'javascript';
         'json';
         'jsonc';
-        'lua';
+        'markdown';
+        'markdown_inline';
         'python';
         'rust';
         'svelte';
@@ -42,83 +26,3 @@ require('nvim-treesitter.configs').setup{
         'typescript';
     }
 }
-
-vim.g.markdown_fenced_languages = {
-    "ts=typescript"
-}
-
--- lsp config
-local lsp = require('lspconfig')
-local capabilities = require('cmp_nvim_lsp').default_capabilities(vim.lsp.protocol.make_client_capabilities())
-capabilities.textDocument.completion.completionItem.snippetSupport = true
-
-attach = function()
-    vim.keymap.set('n', 'K', vim.lsp.buf.hover, {buffer = 0})
-    vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {buffer = 0})
-end
-
-lsp.rust_analyzer.setup{
-    on_attach = attach,
-    capabilities = capabilities,
-}
-lsp.denols.setup{
-    on_attach = attach,
-    capabilities = capabilities,
-}
-lsp.jsonls.setup{}
-lsp.svelte.setup{}
-vim.cmd "autocmd BufWritePre *.rs lua vim.lsp.buf.formatting_sync()" -- rust lsp autoformat
-
--- cmp config
--- Setup nvim-cmp.
-local cmp = require'cmp'
-
-cmp.setup({
-    snippet = {
-        -- REQUIRED - you must specify a snippet engine
-        expand = function(args)
-            -- vim.fn["vsnip#anonymous"](args.body) -- For `vsnip` users.
-            require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
-            -- vim.fn["UltiSnips#Anon"](args.body) -- For `ultisnips` users.
-            -- require'snippy'.expand_snippet(args.body) -- For `snippy` users.
-        end,
-    },
-    mapping = {
-        ['<C-b>'] = cmp.mapping(cmp.mapping.scroll_docs(-4), { 'i', 'c' }),
-        ['<C-f>'] = cmp.mapping(cmp.mapping.scroll_docs(4), { 'i', 'c' }),
-        ['<C-Space>'] = cmp.mapping(cmp.mapping.complete(), { 'i', 'c' }),
-        ['<C-y>'] = cmp.config.disable, -- Specify `cmp.config.disable` if you want to remove the default `<C-y>` mapping.
-        ['<C-e>'] = cmp.mapping({
-            i = cmp.mapping.abort(),
-            c = cmp.mapping.close(),
-        }),
-        ['<CR>'] = cmp.mapping.confirm({ select = true }),
-    },
-    sources = cmp.config.sources({
-        { name = 'nvim_lua' },
-        { name = 'nvim_lsp' },
-        -- { name = 'vsnip' }, -- For vsnip users.
-        { name = 'luasnip' }, -- For luasnip users.
-        -- { name = 'ultisnips' }, -- For ultisnips users.
-        -- { name = 'snippy' }, -- For snippy users.
-    }, {
-        { name = 'buffer' },
-    })
-})
-
--- Use buffer source for `/` (if you enabled `native_menu`, this won't work anymore).
-cmp.setup.cmdline('/', {
-    sources = {
-        { name = 'buffer' }
-    }
-})
-
--- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
-cmp.setup.cmdline(':', {
-    sources = cmp.config.sources({
-        { name = 'path' }
-    }, {
-        { name = 'cmdline' }
-    })
-})
-
