e3a18b206ec8e01cc353580ce54b61f1f6018975
jcvar <36166613+jcvar@users.noreply.github.com>
2026-01-20 23:14:17 -0500

    Restore Tab completion

--- nvim/lua/init.lua
+++ nvim/lua/init.lua
@@ -30,4 +30,5 @@ require('nvim-treesitter.configs').setup{
 
 -- mini.completion config
 require('mini.completion').setup()
-vim.keymap.set('i', '<Tab>', [[pumvisible() ? "\<C-y>" : "\<Tab>"]], { expr = true })
+vim.keymap.set('i', '<Tab>', [[pumvisible() ? "\<C-n>" : "\<Tab>"]], { expr = true })
+vim.keymap.set('i', '<S-Tab>', [[pumvisible() ? "\<C-p>" : "\<S-Tab>"]], { expr = true })
